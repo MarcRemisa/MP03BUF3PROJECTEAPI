2311d329fc561e9bf0a92dc645d144b81d1b287c
MarcRemisa <marc10remisa@gmail.com>
2024-05-10 18:29:23 +0200

    act_finalizada

--- ACT1_M03_UF3/actividad/atronautas/obj/Debug/net8.0/atronautas.AssemblyInfo.cs
+++ ACT1_M03_UF3/actividad/atronautas/obj/Debug/net8.0/atronautas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("atronautas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f03d7e1e97db12d894aab2b3cc7c0e9e8b6d67a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("atronautas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("atronautas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
